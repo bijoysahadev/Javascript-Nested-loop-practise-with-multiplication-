19472ce470ee4ab49c792c2e26f0f1fda4924c62
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-03-14 21:24:18 +0600

    Initial commit for multiplication project

---
+++ index.js
@@ -0,0 +1,22 @@
+// loop are three types=for loop,while loop,do while loop
+// mutlipliaction table of  with for loop
+// let namta= prompt ("Enter A Number")
+// for ( i=1; i<=10; i++)
+// {console.log(namta + " X " + i + " = " +namta*i) ;
+
+// }
+// multipliaction with do while
+// let namta = prompt("Enter a number")
+// i=1
+// while (i<=10) {
+//     console.log(namta + " X " + i + " = "  + namta*i);
+//     i++
+    
+// }
+// mutliplication with do while 
+let namta =prompt("Enter Your Number")
+i=1
+do {console.log(namta + " X " + " = " + namta*i);
+    i++
+}
+while(i<=10)
